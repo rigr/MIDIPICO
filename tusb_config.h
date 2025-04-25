57df47142da9c023727bae0e3a3ad9f6865a61d5
rigr <richard.grauss@gmail.com>
2025-04-25 22:44:11 +0200

    Update tusb_config.h
    host and device active

--- tusb_config.h
+++ tusb_config.h
@@ -1,27 +1,44 @@
-#ifndef TUSB_CONFIG_H
-#define TUSB_CONFIG_H
+#ifndef _TUSB_CONFIG_H_
+#define _TUSB_CONFIG_H_
 
-// Enable both Host and Device modes
-#define CFG_TUSB_RHPORT0_MODE (OPT_MODE_HOST | OPT_MODE_DEVICE)
-#define CFG_TUSB_OS OPT_OS_PICO
+//------------------------------------------------------------------------------
+// Common OPT settings
+//------------------------------------------------------------------------------
+#define CFG_TUSB_MCU        OPT_MCU_RP2040
+#define CFG_TUSB_OS         OPT_OS_PICO
 
-// USB Device configuration
-#define CFG_TUD_ENABLED 1
-#define CFG_TUD_MIDI 1
-#define CFG_TUD_MIDI_RX_BUFSIZE 128
-#define CFG_TUD_MIDI_TX_BUFSIZE 128
+//------------------------------------------------------------------------------
+// Host / Device Enable
+//------------------------------------------------------------------------------
+#define CFG_TUD_ENABLED     1     // TinyUSB device stack
+#define CFG_TUH_ENABLED     1     // TinyUSB host stack
 
-// USB Host configuration
-#define CFG_TUH_ENABLED 1
-#define CFG_TUH_MAX_SPEED OPT_MODE_FULL_SPEED
-#define CFG_TUH_MIDI_ENABLED 1 // Explicitly enable MIDI host
-#define CFG_TUH_MIDI 2 // Support für zwei MIDI devices
-#define CFG_TUH_MIDI_RX_BUFSIZE 128
-#define CFG_TUH_MIDI_TX_BUFSIZE 128
+//------------------------------------------------------------------------------
+// Device Class activate
+//------------------------------------------------------------------------------
+#define CFG_TUD_MIDI        1     // MIDI Device (USB + UART)
+// andere Device-Klassen ggf. deaktivieren:
+// #define CFG_TUD_CDC      0
+// #define CFG_TUD_HID      0
 
-// Common configuration
-#define CFG_TUSB_MCU OPT_MCU_RP2040
-#define CFG_TUSB_MEM_SECTION
-#define CFG_TUSB_MEM_ALIGN __attribute__((aligned(4)))
+//------------------------------------------------------------------------------
+// Host Class activate
+//------------------------------------------------------------------------------
+#define CFG_TUH_MIDI        1     // MIDI Host
+// andere Host-Klassen ggf. deaktivieren:
+// #define CFG_TUH_CDC      0
+// #define CFG_TUH_HID      0
 
-#endif // TUSB_CONFIG_H
+//------------------------------------------------------------------------------
+// Debug
+//------------------------------------------------------------------------------
+#define CFG_TUSB_DEBUG      0
+
+//------------------------------------------------------------------------------
+// FIFO sizes, etc.
+//------------------------------------------------------------------------------
+// ggf. anpassen, wenn Du große SysEx-Pakete über Host schicken willst:
+// #define CFG_TUH_MIDI_RX_BUFSIZE  256
+// #define CFG_TUH_MIDI_TX_BUFSIZE  256
+
+#endif /* _TUSB_CONFIG_H_ */
