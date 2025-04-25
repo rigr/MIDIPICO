34025c238bfedb4b93d13bcc60d1100882480498
rigr <richard.grauss@gmail.com>
2025-04-25 22:49:14 +0200

    Update tusb_config.h

--- tusb_config.h
+++ tusb_config.h
@@ -1,43 +1,27 @@
 #ifndef _TUSB_CONFIG_H_
 #define _TUSB_CONFIG_H_
 
-//------------------------------------------------------------------------------
-// Common OPT settings
-//------------------------------------------------------------------------------
 #define CFG_TUSB_MCU        OPT_MCU_RP2040
 #define CFG_TUSB_OS         OPT_OS_PICO
 
-//------------------------------------------------------------------------------
-// Host / Device Enable
-//------------------------------------------------------------------------------
-#define CFG_TUD_ENABLED     1     // TinyUSB device stack
-#define CFG_TUH_ENABLED     1     // TinyUSB host stack
+// Device + Host Stack
+#define CFG_TUD_ENABLED     1
+#define CFG_TUH_ENABLED     1
 
-//------------------------------------------------------------------------------
-// Device Class activate
-//------------------------------------------------------------------------------
-#define CFG_TUD_MIDI        1     // MIDI Device (USB + UART)
-// andere Device-Klassen ggf. deaktivieren:
+// Device-Klassen
+#define CFG_TUD_MIDI        1    // MIDI Device (tud_midi_*)
+// z.B. CDC, HID usw. hier deaktivieren, falls nicht gebraucht
 // #define CFG_TUD_CDC      0
 // #define CFG_TUD_HID      0
 
-//------------------------------------------------------------------------------
-// Host Class activate
-//------------------------------------------------------------------------------
-#define CFG_TUH_MIDI        1     // MIDI Host
-// andere Host-Klassen ggf. deaktivieren:
+// Host-Klassen
+#define CFG_TUH_MIDI        1    // MIDI Host (tuh_midi_*)
 // #define CFG_TUH_CDC      0
 // #define CFG_TUH_HID      0
 
-//------------------------------------------------------------------------------
-// Debug
-//------------------------------------------------------------------------------
 #define CFG_TUSB_DEBUG      0
 
-//------------------------------------------------------------------------------
-// FIFO sizes, etc.
-//------------------------------------------------------------------------------
-// ggf. anpassen, wenn Du große SysEx-Pakete über Host schicken willst:
+// ggf. Puffergrößen anpassen
 // #define CFG_TUH_MIDI_RX_BUFSIZE  256
 // #define CFG_TUH_MIDI_TX_BUFSIZE  256
 
