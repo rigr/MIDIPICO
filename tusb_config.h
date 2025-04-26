07a1df9ffa60ff59125b8c3ec1822eafe80f0ab5
rigr <richard.grauss@gmail.com>
2025-04-26 12:24:16 +0200

    Update tusb_config.h
    h_midi_receive ist nicht deklariert

--- tusb_config.h
+++ tusb_config.h
@@ -1,8 +1,15 @@
 #ifndef TUSB_CONFIG_H
 #define TUSB_CONFIG_H
 
+// USB Host-Modus aktivieren
 #define CFG_TUSB_RHPORT0_MODE OPT_MODE_HOST
 #define CFG_TUH_ENABLED       1
-#define CFG_TUH_MIDI          2 // Zwei USB-MIDI-Geräte
+
+// Zwei USB-MIDI-Geräte
+#define CFG_TUH_MIDI          2
+
+// Standard-Puffergrößen
+#define CFG_TUSB_MEM_SECTION
+#define CFG_TUSB_MEM_ALIGN   __attribute__((aligned(4)))
 
 #endif
