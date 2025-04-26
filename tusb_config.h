752cbcbb1b3a71db1860ef43a80d56351a8ea327
rigr <richard.grauss@gmail.com>
2025-04-26 14:21:48 +0200

    Update tusb_config.h
    Add tusb_config.h for 2 USB-MIDI hosts and 1 device

--- tusb_config.h
+++ tusb_config.h
@@ -1,14 +1,11 @@
 #ifndef TUSB_CONFIG_H
 #define TUSB_CONFIG_H
 
-// USB Host-Modus aktivieren
-#define CFG_TUSB_RHPORT0_MODE OPT_MODE_HOST
+#define CFG_TUSB_RHPORT0_MODE (OPT_MODE_HOST | OPT_MODE_DEVICE)
 #define CFG_TUH_ENABLED       1
-
-// Zwei USB-MIDI-Geräte
+#define CFG_TUD_ENABLED       1
 #define CFG_TUH_MIDI          2
-
-// Standard-Puffergrößen
+#define CFG_TUD_MIDI          1
 #define CFG_TUSB_MEM_SECTION
 #define CFG_TUSB_MEM_ALIGN   __attribute__((aligned(4)))
 
