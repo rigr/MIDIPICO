9136354ec682c7b262877d2e3eeb08fdab4d6f19
rigr <richard.grauss@gmail.com>
2025-04-26 09:06:12 +0200

    Create tusb_config.h

---
+++ tusb_config.h
@@ -0,0 +1,8 @@
+#ifndef TUSB_CONFIG_H
+#define TUSB_CONFIG_H
+
+#define CFG_TUSB_RHPORT0_MODE OPT_MODE_HOST
+#define CFG_TUH_ENABLED       1
+#define CFG_TUH_MIDI          2 // Zwei USB-MIDI-Geräte
+
+#endif
